c8b648188ebbff120a75f792df0baad6cb72260f
MateoKania <mateotomaskania@gmail.com>
2025-11-07 00:37:00 +0100

    poner clases

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,11 @@
-import daisyui from "daisyui";
+// tailwind.config.js
+const daisyui = require("daisyui");
 
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    "bg-gradient-to-b",
-    "from-yellow-200",
-    "to-blue-400",
-    "from-blue-200",
-    "to-gray-300",
-    "from-gray-200",
-    "to-gray-400",
-    "from-gray-400",
-    "to-gray-600",
-    "from-blue-300",
-    "to-gray-500",
-    "from-blue-400",
-    "to-gray-700",
-    "from-gray-700",
-    "to-black",
-    "from-white",
-    "to-blue-200",
-    "from-gray-300",
-    "to-gray-500",
-    "bg-gray-200",
-    "text-gray-800",
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./tailwind-preserve-classes.html",
   ],
   theme: {
     extend: {
